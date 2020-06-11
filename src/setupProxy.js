dc2ff620be8f8701a991535cc8a7675e65bc62eb
yangheechan <ryanhe4@gmail.com>
2020-06-12 01:42:24 +0900

    changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
       createProxyMiddleware('/api', {
-        target: 'https://peaceful-shelf-28006.herokuapp.com/',
+        target: 'http://peaceful-shelf-28006.herokuapp.com/',
         changeOrigin: true
       }));
 };
