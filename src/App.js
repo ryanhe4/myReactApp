410b3c8dcaaef4f403bb63e4a5a88d6392d494fb
yangheechan <ryanhe4@gmail.com>
2020-06-26 22:04:18 +0900

    change entire structs

--- src/App.js
+++ src/App.js
@@ -1,82 +1,12 @@
 import React, {Component} from 'react';
-
 import axios from 'axios';
-//import FileSaver from 'file-saver';
-import storage from './lib/storage';
 
 class App extends Component {
   state = {
     url: '',
     email: '',
-    email2: '',
-    email3: '',
     run: false,
-    sendlist: [],
-  };
-
-  registerfunc = () => {
-    //등록
-    if (!this.state.loopRet) {
-      this.handleCheckData();
-
-      const loopRet = setInterval(this.handleCheckData, 1000 * 60 * 10);
-      this.setState({
-        loopRet,
-        run: true,
-      });
-    }
-  };
-  clearfunc = () => {
-    clearInterval(this.state.loopRet);
-
-    this.setState({
-      run: false,
-      loopRet: '',
-    });
-  };
-
-  handleCheckData = async () => {
-    //local Storage Confirm
-    const dateString = storage.get('__DATE_STRING__');
-
-    if (!dateString) {
-      console.log('저장된 date 없음');
-      storage.set('__DATE_STRING__', {dateString: 0});
-    }
-    //email 코드
-    const {email, email2, email3} = this.state;
-    const emailstring = email + ',' + email2 + ',' + email3;
-
-    console.log(emailstring);
-
-    try {
-      const check = await axios.post('/api/v1.0/crawler/getlink', {
-        uri: this.state.url,
-        dateString: dateString.dateString || 0,
-        emails:emailstring,
-      });
-
-      console.log(check);
-
-      if (check.data['check'] === true) {
-        //local Storage set;
-        storage.set('__DATE_STRING__', {dateString: check.data['dateString']});
-
-        const {sendlist} = this.state;
-
-        sendlist.push({
-          no:check.data['dateString'],
-        })
-
-        this.setState({
-          sendlist,
-        })
-      } else {
-        console.log('새로운 데이터 없음');
-      }
-    } catch (e) {
-      console.error(e);
-    }
+    emails: [],
   };
 
   inputChange = (e) => {
@@ -84,15 +14,28 @@ class App extends Component {
       [e.target.id]: e.target.value,
     });
   };
+  handleUrlButtonClick = async () => {
+    const {url} = this.state;
+
+    const checkUrl = await axios.post('/api/v1.0/crawler/check', {url});
+    console.log(checkUrl);
+
+    if (checkUrl.data.exists) {
+      this.setstate({
+        emails: checkUrl.data.emails,
+        run:true,
+      });
+    }
+  };
 
   render() {
-    const printlist = this.state.sendlist.map(
+    const printlist = this.state.emails.map(
         item => {
-          return(
-            <div key={item.no}>
-              {item.no}
-            </div>
-          )
+          return (
+              <div>
+                {item}
+              </div>
+          );
         },
     );
 
@@ -100,21 +43,18 @@ class App extends Component {
         <div className="App">
           <h2> URL 입력 </h2>
           <input onChange={this.inputChange} id="url"/>
-          {
-            !this.state.run &&
-            (<button onClick={this.registerfunc}> 시작</button>)
-          }
-          {
-            this.state.run && (<button onClick={this.clearfunc}> 중지</button>)
-          }
+          <button onClick={this.handleUrlButtonClick}> 불러오기</button>
           <hr/>
-          <h2> 이메일 입력 </h2>
-          <input placeholder={'이메일1'} onChange={this.inputChange} id="email"/>
-          <input placeholder={'이메일2'} onChange={this.inputChange} id="email2"/>
-          <input placeholder={'이메일3'} onChange={this.inputChange} id="email3"/>
-          <hr/>
-          <h2> log </h2>
-          {printlist}
+          <h2> 이메일 리스트 </h2>
+          {
+            this.state.run && (
+                <input placeholder={'이메일'} onChange={this.inputChange}
+                       id="email"/>
+            )
+            && (
+                {printlist}
+            )
+          }
         </div>
     );
   }
--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-// const {createProxyMiddleware} = require('http-proxy-middleware');
-//
-// module.exports = function(app) {
-//   app.use(
-//       createProxyMiddleware('/api', {
-//         target: 'http://peaceful-shelf-28006.herokuapp.com/',
-//         changeOrigin: true
-//       }));
-// };
